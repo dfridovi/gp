a8484575271c08be61829f3b6670a20c4f83df4d
David Fridovich-Keil <david.fridovichkeil@gmail.com>
2017-03-31 20:45:31 -0700

    Automated documentation build for changeset 8c0e817be4047925e8b6978d3280d702b0d72121.

--- documentation/html/search/pages_0.js
+++ documentation/html/search/pages_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['gaussianprocess',['GaussianProcess',['../classgp_1_1_gaussian_process.html',1,'gp']]],
   ['gaussian_20processes',['Gaussian Processes',['../index.html',1,'']]]
 ];
